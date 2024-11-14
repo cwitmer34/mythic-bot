66a5a88bbaa9e90e07b2c1ce63d76c14c3cc024d
Connor <connorwitmer34@gmail.com>
2024-11-14 06:50:15 -0500

    implement quit logic

--- src/commands/SlashReviewApps/staffReviewRegApps.js
+++ src/commands/SlashReviewApps/staffReviewRegApps.js
@@ -2,7 +2,10 @@ const { client, appsBeingReviewed } = require("../../index");
 const mongo = require("../../mongo"); // Ensure this is correct
 const mongoose = require("mongoose");
 const config = require("../../config.json");
-const { currentAppEmbed } = require("../../util/embeds/registrationAppReview");
+const { currentAppEmbed, endReviewEmbed } = require("../../util/embeds/registrationAppReview");
+const { ActionRowBuilder } = require("@discordjs/builders");
+const { ButtonBuilder } = require("@discordjs/builders");
+const { ButtonStyle } = require("discord.js");
 
 client.on("interactionCreate", async (interaction) => {
   if (interaction.isCommand() && interaction.commandName === "reviewapps" && !interaction.user.bot)
@@ -18,7 +21,7 @@ client.on("interactionCreate", async (interaction) => {
     handleNextApplication(interaction);
 
   if (interaction.isButton() && interaction.customId === "quitRegApp" && !interaction.user.bot)
-    interaction.message.delete();
+    endReviewProcess(interaction);
 });
 
 async function startReviewProcess(interaction) {
@@ -28,6 +31,7 @@ async function startReviewProcess(interaction) {
     appsBeingReviewed.set(interaction.user.id, currentApps);
     interaction.reply({
       embeds: [currentAppEmbed(currentApps[0])],
+      components: [reviewRow],
     });
   } catch (error) {
     console.error("Error fetching registration apps:", error);
@@ -35,7 +39,10 @@ async function startReviewProcess(interaction) {
 }
 
 async function endReviewProcess(interaction) {
-  appsBeingReviewed.delete(interaction.user.id);
+  interaction.update({
+    embeds: [endReviewEmbed(interaction.user.tag)],
+    components: [],
+  });
 }
 
 async function approveApplication(interaction) {
@@ -45,3 +52,20 @@ async function approveApplication(interaction) {
 async function denyApplication(interaction) {}
 
 async function handleNextApplication(interaction) {}
+
+const reviewRow = new ActionRowBuilder()
+  .addComponents(
+    new ButtonBuilder()
+      .setCustomId("approveRegApp")
+      .setLabel("Approve")
+      .setStyle(ButtonStyle.Success)
+  )
+  .addComponents(
+    new ButtonBuilder().setCustomId("denyRegApp").setLabel("Deny").setStyle(ButtonStyle.Danger)
+  )
+  .addComponents(
+    new ButtonBuilder().setCustomId("skipRegApp").setLabel("Skip").setStyle(ButtonStyle.Primary)
+  )
+  .addComponents(
+    new ButtonBuilder().setCustomId("quitRegApp").setLabel("Quit").setStyle(ButtonStyle.Secondary)
+  );
